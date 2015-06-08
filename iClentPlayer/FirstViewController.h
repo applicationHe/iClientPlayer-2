58b35ae88914228d1a8a384c98928a1af7ed0a73
LimZhong <limzhong@LimZhongdeMac.local>
2015-06-08 09:27:00 +0800

    Initial Commit

---
+++ iClentPlayer/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  iClentPlayer
+//
+//  Created by LimZhong on 15-6-8.
+//  Copyright (c) 2015年 LimZhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ iClentPlayer/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  iClentPlayer
+//
+//  Created by LimZhong on 15-6-8.
+//  Copyright (c) 2015年 LimZhong. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
